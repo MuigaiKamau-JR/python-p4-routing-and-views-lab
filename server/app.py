2e6bad4b36e7c6c755300e5fbd87b60c4316cac2
Kamau <kamauwamuigaijr@gmail.com>
2024-06-28 23:47:26 +0300

    complete lab

--- server/app.py
+++ server/app.py
@@ -1,8 +1,53 @@
-#!/usr/bin/env python3
-
 from flask import Flask
 
 app = Flask(__name__)
 
+@app.route('/')
+def index():
+    return '<h1>Python Operations with Flask Routing and Views</h1>'
+
+@app.route('/print/<string:parameter>')
+def print_string(parameter):
+    print(parameter)  # Print to console
+    return parameter  # Return the plain string
+
+@app.route('/count/<int:parameter>')
+def count(parameter):
+    numbers = '\n'.join(str(num) for num in range(parameter))
+    return f'<pre>{numbers}</pre>'
+
+@app.route('/math/<int:num1>/<operation>/<int:num2>')
+def math(num1, operation, num2):
+    if operation == '+':
+        result = num1 + num2
+    elif operation == '-':
+        result = num1 - num2
+    elif operation == '*':
+        result = num1 * num2
+    elif operation == 'div':
+        if num2 != 0:
+            result = num1 / num2
+        else:
+            return 'Error: Division by zero!'
+    elif operation == '%':
+        result = num1 % num2
+    else:
+        return 'Error: Invalid operation!'
+
+    return f'<h1>Result: {result}</h1>'
+
 if __name__ == '__main__':
-    app.run(port=5555, debug=True)
+    app.run(port=5555)
+
+
+
+
+
+# #!/usr/bin/env python3
+
+# from flask import Flask
+
+# app = Flask(__name__)
+
+# if __name__ == '__main__':
+#     app.run(port=5555, debug=True)
